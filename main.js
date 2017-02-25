7d08c802813757e7e0dd4a3ab12df9242bb6316a
Ben Smith <ben@vectormediagroup.com>
2017-02-25 15:55:25 -0500

    ugly version

--- main.js
+++ main.js
@@ -1,154 +1,184 @@
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
-const url = require('url')
+const {app, BrowserWindow, ipcMain} = require('electron');
+const path = require('path');
+const url = require('url');
 const sqlite3 = require('sqlite3');
 const fs = require('fs');
-const Promise = require('bluebird');
+const promise = require('bluebird');
 const request = require('request');
 
-Promise.promisifyAll(require("request"));
+promise.promisifyAll(require("request"));
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let win
+let win;
 
-function createWindow () {
+let initApp = () => {
+
+    let dbSourcePath = ( () => {
+        if (process.platform === 'darwin') {
+            let profileDirs = ['Default', 'Profile 1'];
+            for (var i = 0; i < profileDirs.length; i++) {
+                let path = process.env.HOME + '/Library/Application Support/Google/Chrome/'+profileDirs[i]+'/History';
+                if (fs.existsSync(path)) {
+                    return path;
+                }
+            }
+            return '';
+        } else {
+            // todo
+            return '';
+        }
+    })();
+
+    let dbTempPath = '/tmp/Chrome_history';
+
+    var copyDb = () => {
+        return new promise( (resolve, reject) => {
+            if (dbSourcePath==='') {
+                reject('Chrome history could not be found.');
+            }
+            var copy = fs.createReadStream(dbSourcePath).pipe(fs.createWriteStream(dbTempPath));
+            copy.on('finish', () => {
+                resolve();
+            });
+        });
+    };
+
+    var readDb = () => {
+        return new promise( (resolve, reject) => {
+            var db = new sqlite3.Database(dbTempPath);
+            db.serialize(() => {
+                db.all("SELECT DISTINCT(url) FROM urls;", function(err, rows) {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(rows);
+                    }
+                });
+            });
+            db.close();
+        });
+    };
+
+    var isHostUsingCloudFlare = (url) => {
+        return request.getAsync({
+            method: 'head',
+            url: url,
+            timeout: 5000,
+        })
+        .then(function(response) {
+            return Object.keys(response.headers).indexOf('cf-ray') !== -1 ? true : false;
+        })
+        .catch(function(err) {
+        });
+    };
+
+    var parseHistoryForUniqueHostnames = (rows) => {
+        let hostnames = [];
+        var hostChecks = [];
+        return new promise((resolve, reject) => {
+            for (var i = 0; i < rows.length; i++) {
+                var hostname = url.parse(rows[i].url).hostname;
+                if (hostnames.indexOf(hostname)===-1) {
+                    hostnames.push(hostname);
+                    hostChecks.push('http://' + hostname);
+                    hostChecks.push('https://' + hostname);
+                }
+                // if (i===500) break;
+            }
+            resolve(hostChecks);
+        });
+    };
+
+    var determineCloudFlareHosts = (hostChecks) => {
+        let numberOfHostsToCheck = hostChecks.length;
+        let completed = 0;
+        this.cloudFlareHosts = [];
+        return promise.map(hostChecks, (host) => {
+            completed++;
+            percentComplete = (completed) / numberOfHostsToCheck;
+            win.webContents.send('updateCounter', percentComplete);
+            return isHostUsingCloudFlare(host).then((isCloudFlare) => {
+                if (isCloudFlare) {
+                    let report = url.parse(host).hostname;
+                    if (this.cloudFlareHosts.indexOf(report)===-1) {
+                        this.cloudFlareHosts.push(report);
+                    }
+                }
+                Promise.resolve();
+            });
+        }, { concurrency: 100 });
+    };
+
+    var reportToRenderer = () => {
+        this.cloudFlareHosts.sort();
+        return new Promise((resolve, reject) => {
+            win.webContents.send('reportHosts', this.cloudFlareHosts);
+        });
+    };
+
+    var clerTempDb = () => {
+        return new promise(function( resolve, reject) {
+            fs.unlink(dbTempPath, function(err) {
+                resolve();
+            });
+        });
+    };
+
+    copyDb()
+        .bind({})
+        .then(readDb)
+        .then(parseHistoryForUniqueHostnames)
+        .then(determineCloudFlareHosts)
+        .then(reportToRenderer)
+        .then(clerTempDb)
+        .catch((err) => {
+            win.webContents.send('displayMsg', err);
+        });
+};
+
+let createWindow = () => {
   // Create the browser window.
-  win = new BrowserWindow({width: 400, height: 400})
+  win = new BrowserWindow({width: 750, height: 600});
 
   // and load the index.html of the app.
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
     slashes: true
-  }))
+  }));
 
   // Open the DevTools.
-  // win.webContents.openDevTools()
+  // win.webContents.openDevTools();
 
   // Emitted when the window is closed.
   win.on('closed', () => {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    win = null
-  })
-}
+    win = null;
+  });
+};
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createWindow);
+app.on('ready', initApp);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
 
 app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (win === null) {
-    createWindow()
+    createWindow();
   }
-})
-
-
-// // // // // // // // // // // // // // // // // // // // // // // //
-// // // // // // // // // // // // // // // // // // // // // // // //
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
-
-let dbSourcePath = (function() {
-    if (process.platform === 'darwin') {
-        return process.env.HOME + '/Library/Application Support/Google/Chrome/Default/History';
-    } else {
-        // todo
-        return '';
-    }
-})();
-
-let dbTempPath = '/tmp/Chrome_history';
-
-var copyDb = function() {
-    return new Promise(function(resolve, reject) {
-        var copy = fs.createReadStream(dbSourcePath).pipe(fs.createWriteStream(dbTempPath));
-        copy.on('finish', function() {
-            resolve();
-        });
-    });
-}
-
-var readDb = function() {
-    return new Promise(function(resolve, reject) {
-        var db = new sqlite3.Database(dbTempPath);
-        db.serialize(function() {
-            db.all("SELECT DISTINCT(url) FROM urls;", function(err, rows) {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(rows);
-                }
-            });
-        });
-        db.close();
-    });
-}
-
-var isHostUsingCloudFlare = function(url) {
-    return request.getAsync({
-        method: 'head',
-        url: url,
-        timeout: 3000,
-    })
-    .then(function(response) {
-        return Object.keys(response.headers).indexOf('cf-ray') !== -1 ? url : false;
-    })
-    .catch(function(err) {
-        // console.log(err);
-    });
-}
-
-var parseHistoryForUniqueHostnames = function(rows) {
-    let hostnames = [];
-    var hostChecks = [];
-    return new Promise(function(resolve, reject) {
-        for (var i = 0; i < rows.length; i++) {
-            var hostname = url.parse(rows[i].url).hostname;
-            if (hostnames.indexOf(hostname)===-1) {
-                hostnames.push(hostname);
-                hostChecks.push(isHostUsingCloudFlare('http://' + hostname));
-                hostChecks.push(isHostUsingCloudFlare('https://' + hostname));
-            }
-            // if (i===500) break;
-        }
-        resolve(hostChecks);
-    });
-}
-
-var determineCloudFlareHosts = function(hostChecks) {
-    var cloudFlareHosts = [];
-    return Promise.each(hostChecks, function(host) {
-        if (!host || host===false) return;
-        cloudFlareHosts.push(url.parse(host).hostname);
-    });
-}
-
-var clerTempDb = function() {
-    return new Promise(function( resolve, reject) {
-        fs.unlink(dbTempPath, function(err) {
-            resolve();
-        });
-    });
-}
-
-copyDb()
-    .then(readDb)
-    .then(parseHistoryForUniqueHostnames)
-    .then(determineCloudFlareHosts)
-    .then(clerTempDb);
+});
